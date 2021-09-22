f5f3e9199cd868ed7b727822a90c2545688a93c8
Luke Bedrosian <bedrosian.luke05@gmail.com>
2021-09-22 16:13:10 -0400

    commit tree 45df681db0f3f70598cd0552698d5bf50c4375e8

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let changes = document.getElementById('text')
+    changes.textContent = "This is really cool!"
+ })
